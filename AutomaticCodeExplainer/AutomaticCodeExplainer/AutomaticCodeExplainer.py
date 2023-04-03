2718fe0f024ff3ce0458878819f72bd1b55a35cc
Jacky Hsiao <jacky.hsiao@wiadvance.com>
2023-04-04 01:02:15 +0800

    fix:無法使用 code-davinci-002 feature: 新增中英文提示選項

--- AutomaticCodeExplainer/AutomaticCodeExplainer/AutomaticCodeExplainer.py
+++ AutomaticCodeExplainer/AutomaticCodeExplainer/AutomaticCodeExplainer.py
@@ -8,15 +8,24 @@ openai.api_key = os.environ.get("OPEN_API_KEY")
 def hello(name):
     print(f"Hello {name}")
 
-def docstring_prompt(code):
-    prompt = f"{code}\n # 幫 python function 使用中文加上程式碼註解:\n '''"
+def docstring_prompt_zh_tw(code):
+    prompt = f"{code}\n # 幫 python function 使用中文加上程式碼說明，包含功能、參數以及回傳值，保留原本程式碼內容:\n '''"
     return prompt
 
-response = openai.Completion.create(
-    model = 'code-davinci-002',
-    prompt = docstring_prompt(inspect.getsource(hello)),
-    temperature =0,
-    max_tokens = 100,
-    top_p = 1.0,
-    stop = ["'''"])
-print(response['choices'][0]['text'])
+def docstring_prompt(code):
+    prompt = f"{code}\n # A high quality python docstring of the above python function:\n '''"
+    return prompt
+
+def get_docstrings(docstring_prompt_function,function_name):
+    response = openai.Completion.create(
+        model = 'text-davinci-003',
+        prompt = docstring_prompt_function(inspect.getsource(function_name)),
+        temperature =0,
+        max_tokens = 100,
+        top_p = 1.0,
+        stop = ["'''"])
+    print(response['choices'][0]['text'])
+    print('----------------------------')
+
+get_docstrings(docstring_prompt_zh_tw, hello)
+get_docstrings(docstring_prompt, hello)
