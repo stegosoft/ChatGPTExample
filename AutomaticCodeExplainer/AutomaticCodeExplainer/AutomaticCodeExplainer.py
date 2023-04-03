d36386a956cd3f09021652df891a3a167f305d28
Jacky Hsiao <jacky.hsiao@wiadvance.com>
2023-04-04 01:42:42 +0800

    section: 39th 將註解加至程式碼中 使用提示新增4個空白縮排 消除 open ai api 回傳值換行後面多一個空白

--- AutomaticCodeExplainer/AutomaticCodeExplainer/AutomaticCodeExplainer.py
+++ AutomaticCodeExplainer/AutomaticCodeExplainer/AutomaticCodeExplainer.py
@@ -9,12 +9,19 @@ def hello(name):
     print(f"Hello {name}")
 
 def docstring_prompt_zh_tw(code):
-    prompt = f"{code}\n # 幫 python function 使用中文加上程式碼說明，包含功能、參數以及回傳值，保留原本程式碼內容:\n '''"
+    prompt = f"{code}\n # 幫 python function 使用中文加上程式碼說明，包含功能、參數以及回傳值，保留原本程式碼內容，所產內容每行都加上四個空白:\n '''"
     return prompt
 
 def docstring_prompt(code):
-    prompt = f"{code}\n # A high quality python docstring of the above python function:\n '''"
+    prompt = f"{code}\n # A high quality python docstring of the above python function, before docstring add 4 space:\n '''"
     return prompt
+
+def merge_docstring_and_function(original_function, docstring):
+    function_string = inspect.getsource(original_function)
+    split = function_string.split('\n')
+    first_part, second_part = split[0], split[1:]
+    merged_function = f"{first_part}\n    '''{docstring.rstrip(' ')}    '''\n"+'\n'.join(second_part)
+    return merged_function
 
 def get_docstrings(docstring_prompt_function,function_name):
     response = openai.Completion.create(
@@ -24,8 +31,10 @@ def get_docstrings(docstring_prompt_function,function_name):
         max_tokens = 100,
         top_p = 1.0,
         stop = ["'''"])
-    print(response['choices'][0]['text'])
+    print(merge_docstring_and_function(function_name,response['choices'][0]['text']))
     print('----------------------------')
+
+
 
 get_docstrings(docstring_prompt_zh_tw, hello)
 get_docstrings(docstring_prompt, hello)
